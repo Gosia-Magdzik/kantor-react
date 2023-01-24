bcaca5df95ed2c90113e6929d3c701ab261f6654
Gosia Magdzik <melassa26@gmail.com>
2023-01-24 12:55:49 +0100

    js-... delete

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@
 
 function App() {
   return (
-    <form className="js-form">
+    <form >
     <fieldset className="fieldset">
         <legend className="fieldset__legend">Kalkulator walut</legend>
         <p>
             <span className="fieldset__currency" for="zl">Kwota: </span>
-            <input className="fieldset__output js-amount" type="number" step="0.1" name="zl" id="zl" required placeholder="Wpisz kwotę w zł"/>
+            <input className="fieldset__output" type="number" step="0.1" name="zl" id="zl" required placeholder="Wpisz kwotę w zł"/>
         </p>
         <p>
             <span className="fieldset__currency">Waluta*:</span>
-            <select className="fieldset__output js-currency">
+            <select className="fieldset__output">
                 <option value="EUR">Euro</option>
                 <option value="GBP">Funt Brytyjski</option>
                 <option value="NOK">Korona Norweska</option>
