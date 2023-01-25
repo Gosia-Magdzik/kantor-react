71b84cf5eb8729f78503cbb5a4041c04104e7345
Gosia Magdzik <melassa26@gmail.com>
2023-01-25 13:10:53 +0100

    Form component added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-
+import Form from "./Form";
 
 function App() {
   return (
-    <form className="body">
-        <fieldset className="fieldset">
-            <legend className="fieldset__legend">Kalkulator walut</legend>
-            <p>
-                <span className="fieldset__currency" for="zl">Kwota: </span>
-                <input className="fieldset__output" type="number" step="0.1" name="zl" id="zl" required placeholder="Wpisz kwotę w zł"/>
-            </p>
-            <p>
-                <span className="fieldset__currency">Waluta*:</span>
-                <select className="fieldset__output">
-                    <option value="EUR">Euro</option>
-                    <option value="GBP">Funt Brytyjski</option>
-                    <option value="NOK">Korona Norweska</option>
-                    <option value="USD">Dolar Amerykańki</option>
-                </select>
-            </p>
-            <p>
-                <button className="fieldset__button">PRZELICZ</button>
-            </p>
-
-            <p className="fieldset__result" for="wynik">Kwota po przeliczeniu:  
-                <strong className="fieldset__output"></strong>
-            </p>
-
-            <p className="fieldset__paragraph">*kurs z 15 grudnia 2022 r. </p>
-        </fieldset>
-    </form>  
+    <Form/>
   );
 }
 
