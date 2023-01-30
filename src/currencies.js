9cdc088507f797f16a55d38cb6e03e83a72f0a75
Gosia Magdzik <melassa26@gmail.com>
2023-01-30 14:24:32 +0100

    + currencies table

---
+++ src/currencies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.7,
+    },
+    {
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 4.42,
+    },
+    {
+        short: "GBP",
+        name: "Funt Brytyjski",
+        rate: 5.39,
+    },
+    {
+        short: "NOK",
+        name: "Korona Norweska",
+        rate: 0.45,
+    },
+];
