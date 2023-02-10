9f61ab02be5eb7c7d22dca9862d6f82b11a448f5
Gosia Magdzik <melassa26@gmail.com>
2023-02-10 16:14:05 +0100

    'Clock' styled

--- src/Clock/styled.js
+++ src/Clock/styled.js
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const StyledClock = styled.div`
     text-align: right;
     font-family: monospace;
-    color: black;
+    color: ${({ theme }) => theme.color.black};
 `;
