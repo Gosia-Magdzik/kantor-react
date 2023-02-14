04cf9158065f45d5d0958c3ac768e6e8cb9583ad
Gosia Magdzik <melassa26@gmail.com>
2023-02-14 14:07:38 +0100

    new useRateData component

--- src/Form/useRateData.js
+++ src/Form/useRateData.js
@@ -1,7 +1,26 @@
 import { useState, useEffect } from "react";
+import axios from "axios"
 
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState ({
         state: "loading",
-    })
-}
+    });
+
+    useEffect(() => {
+        const getRates = async () => {
+            try {
+                    const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+                setRatesData({
+                    date: response.data.date,
+                    rate: response.data.rates,
+                    status: "success",
+                });
+            } catch (error) {
+                setRatesData({
+                    status: "error",
+                });
+            }
+        };
+        setTimeout(getRates, 1000);
+    },  []);
+};
