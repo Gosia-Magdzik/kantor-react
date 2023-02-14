66770b304ab2bdeb3f3187c82a82882f9e48884e
Gosia Magdzik <melassa26@gmail.com>
2023-02-14 11:03:51 +0100

    old currency converted method deleted

--- src/App.js
+++ src/App.js
@@ -4,20 +4,6 @@ import { useState } from "react";
 
 function App() {
 
-  const [result, setResult] = useState();
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies
-      .find(({short}) => short === currency)
-      .rate;
-
-      setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency 
-      });
-  }
-
   return (
         <Form
           result={result}
