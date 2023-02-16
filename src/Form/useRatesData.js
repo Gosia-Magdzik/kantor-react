eb23d9952a9e732859d539c6b5cbd38903a4250a
Gosia Magdzik <melassa26@gmail.com>
2023-02-16 15:45:06 +0100

    'status', not state

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -3,25 +3,26 @@ import axios from "axios"
 
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState ({
-        state: "loading",
+        status: "loading",
     });
 
     useEffect(() => {
         const getRates = async () => {
             try {
-                    const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
-                setRatesData({
-                    date: response.data.date,
-                    rates: response.data.rates,
-                    status: "success",
-                });
+                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN&symbols=USD,CZK,EUR,GBP,JOD,NOK,JPY,AUD,CAD,CHF,CNH,BGN,EGP,ILS&");
+                    setRatesData({
+                        date: response.data.date,
+                        rates: response.data.rates,
+                        status: "success",
+                    });
             } catch (error) {
+                console.error("Some error has occurred", error);
                 setRatesData({
                     status: "error",
                 });
             }
         };
-        setTimeout(getRates, 1000);
+        setTimeout(getRates, 2000);
     },  []);
 
     return ratesData;
